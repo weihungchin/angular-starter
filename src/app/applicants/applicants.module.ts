c574fd147a95e223359ac8057a86469ee6451aab
WeiHung <weihung.chin@sicmscb.com>
2018-03-01 18:35:10 +0800

    angular flex layout

--- src/app/applicants/applicants.module.ts
+++ src/app/applicants/applicants.module.ts
@@ -1,34 +1,41 @@
-import { MatIconModule } from '@angular/material/icon';
-import { ApplicantsRoutingModule } from './applicants-routing.module';
-import { NgModule } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
+import { ApplicantsRoutingModule } from "./applicants-routing.module";
+import { NgModule } from "@angular/core";
 
-import { ApplicantsComponent } from './applicants.component';
-import { MatCardModule } from '@angular/material/card';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatMenuModule } from '@angular/material/menu';
+import { ApplicantsComponent } from "./applicants.component";
+import { MatCardModule } from "@angular/material/card";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatListModule } from "@angular/material/list";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatMenuModule } from "@angular/material/menu";
+import { FilterComponent } from "./filter/filter.component";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ApplicantsRoutingModule,
-        MatCardModule,
-        MatButtonModule,
-        MatIconModule,
-        MatGridListModule,
-        MatListModule,
-        FlexLayoutModule,
-        AngularFireDatabaseModule,
-        MatProgressSpinnerModule,
-        MatMenuModule
-    ],
-    exports: [],
-    declarations: [ApplicantsComponent],
-    providers: [],
+  imports: [
+    CommonModule,
+    ApplicantsRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatListModule,
+    FlexLayoutModule,
+    AngularFireDatabaseModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatTabsModule
+  ],
+  exports: [],
+  declarations: [ApplicantsComponent, FilterComponent],
+  providers: []
 })
-export class ApplicantsModule { }
+export class ApplicantsModule {}
---
+++ src/app/applicants/filter/filter.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatCheckboxChange } from "@angular/material/checkbox";
+import {
+  MatSelectionList,
+  MatSelectionListChange,
+  MatListOption
+} from "@angular/material/list";
+
+@Component({
+  selector: "app-filter",
+  templateUrl: "./filter.component.html",
+  styleUrls: ["./filter.component.scss"]
+})
+export class FilterComponent implements OnInit {
+  @ViewChild("list") selectionList: MatSelectionList;
+
+  itemList: any = [
+    { key: "1", label: "item 1" },
+    { key: "2", label: "item 2" },
+    { key: "3", label: "item 3" }
+  ];
+
+  filterList: any = [
+    {
+      groupKey: "r",
+      groupLabel: "Recruiter",
+      groupList: [
+        { key: "1", label: "item 1" },
+        { key: "2", label: "item 2" },
+        { key: "3", label: "item 3" }
+      ]
+    },
+    {
+      groupKey: "c",
+      groupLabel: "Channel",
+      groupList: [
+        { key: "c1", label: "Channel 1" },
+        { key: "c2", label: "Channel 2" },
+        { key: "c3", label: "Channel 3" }
+      ]
+    },
+    {
+      groupKey: "y",
+      groupLabel: "Years of Experience",
+      groupList: [
+        { key: "y1", label: "0 -1 years" },
+        { key: "y2", label: "2 - 4 years" },
+        { key: "y3", label: "5 - 7 years" },
+        { key: "y4", label: "> 7 years" },
+      ]
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelectChange(e: MatSelectionListChange) {
+    console.log(e.option.value, e.option.selected);
+    // console.log(this.selectionList.selectedOptions.selected);
+    // const arr: MatListOption[] = this.selectionList.selectedOptions.selected;
+    // if (arr && arr.length > 0) {
+    //   console.log(arr[0].value);
+    // }
+  }
+
+  onGroupCheck(e: MatCheckboxChange, list:MatSelectionList) {
+    if (e.checked) {
+      list.selectAll();
+    } else if (!e.checked) {
+      list.deselectAll();
+    }
+  }
+}
